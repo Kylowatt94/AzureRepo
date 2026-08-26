5956395cb7a60de21f4bab1d6d28ab5cdfe651a7
Kylowatt94 <deschampslazaro@gmail.com>
2026-08-25 21:29:26 -0400

    04RemoveLab.ps1

--- 99RemoveLab.ps1
+++ 99RemoveLab.ps1
@@ -1,14 +1,6 @@
 <#
-.SYNOPSIS
-    Teardown: deletes RGTest and everything inside it (VNet, VM, NIC, NSG,
-    public IP, disks - all of it) so you can rebuild the lab from scratch
-    next time you want to practice.
-
-.NOTES
-    This is a hard delete of the whole resource group. There's no confirm
-    prompt beyond -Force below - if you only want to delete one piece
-    (e.g. just the VM to try a redeploy), use the matching Remove-Az* cmdlet
-    instead (Remove-AzVM, Remove-AzVirtualNetwork, etc.).
+NOTES
+    This is a hard delete of the whole resource group.
 #>
 
 $resourceGroupName = "RGTest"
