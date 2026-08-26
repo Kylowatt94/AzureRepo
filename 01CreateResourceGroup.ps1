825d206406786821d227e786a318eb3b51cecbb4
Kylowatt94 <deschampslazaro@gmail.com>
2026-08-25 22:27:08 -0400

    01CreateResourceGroup.ps1

--- 01CreateResourceGroup.ps1
+++ 01CreateResourceGroup.ps1
@@ -1,11 +1,3 @@
-<#
-    Lab step 1: creates the resource group everything else in this lab lives in.
-
-NOTES
-    - Location defaults to "eastus". Change $location if you want a different region.
-    - Run Connect-AzAccount once per PowerShell session before running these scripts.
-#>
-
 $resourceGroupName = "RGTest"
 $location           = "eastus"
 
